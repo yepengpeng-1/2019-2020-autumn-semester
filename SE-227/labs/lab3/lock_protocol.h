2ab42a832c6b0fa26b401621d835a269044933b6
yuetsin <akaza_akari@sjtu.edu.cn>
2019-11-04 19:47:48 +0800

    [add] prepare for idiot lab3 brought by HFQ

--- SE-227/labs/lab3/lock_protocol.h
+++ SE-227/labs/lab3/lock_protocol.h
@@ -12,5 +12,11 @@ public:
     typedef unsigned long long lockid_t;
     enum rpc_numbers { acquire = 0x7001, release, stat };
 };
+class rlock_protocol {
+public:
+    enum xxstatus { OK, RPCERR };
+    typedef int status;
+    enum rpc_numbers { revoke = 0x8001, retry = 0x8002 };
+};
 
 #endif
