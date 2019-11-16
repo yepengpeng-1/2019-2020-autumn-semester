b211df63aeb76e169f09a8a36b9a65678b3a3cf1
yuetsin <akaza_akari@sjtu.edu.cn>
2019-11-16 18:23:42 +0800

    [add] SE-344 preparations

--- SE-342/assignments/ass1/src/ConvolutionExample/convolutionexample.py
+++ SE-342/assignments/ass1/src/ConvolutionExample/convolutionexample.py
@@ -1,15 +1,20 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtGui import QIcon
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+import mainwindow
 
-class ConvolutionExample(QMainWindow):
+class ConvolutionExample():
     def __init__(self):
-        QMainWindow.__init__(self)
-
+        app = QApplication(sys.argv)
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        ui = mainwindow.Ui_MainWindow()
+        ui.setupUi(MainWindow)
+        MainWindow.show()
+        sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = ConvolutionExample()
-    window.show()
-    sys.exit(app.exec_())
+    ConvolutionExample()
