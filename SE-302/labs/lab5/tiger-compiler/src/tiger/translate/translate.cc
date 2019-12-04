3c73c995dd54160765434fc6dc91b6f0a22a53a4
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-04 21:18:50 +0800

    its too hard for me too hard

--- SE-302/labs/lab5/tiger-compiler/src/tiger/translate/translate.cc
+++ SE-302/labs/lab5/tiger-compiler/src/tiger/translate/translate.cc
@@ -1,18 +1,20 @@
 #include "tiger/translate/translate.h"
 
-#include <cstdio>
-#include <iostream>
-#include <set>
-#include <string>
-
 #include "tiger/errormsg/errormsg.h"
 #include "tiger/frame/temp.h"
 #include "tiger/semant/semant.h"
 #include "tiger/semant/types.h"
 #include "tiger/util/util.h"
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <string>
 
 using VEnvType = S::Table< E::EnvEntry >*;
 using TEnvType = S::Table< TY::Ty >*;
+
+namespace {}  // namespace
 
 namespace TR {
 
@@ -179,7 +181,7 @@ static TY::TyList* make_formal_tylist( TEnvType tenv, A::FieldList* params ) {
 
     TY::Ty* ty = tenv->Look( params->head->typ );
     if ( ty == nullptr ) {
-        std::cout << "undefined type %s" << params->head->typ->Name() << std::endl;
+        std::cout << params->head->pos << "undefined type " << params->head->typ->Name() << std::endl;
     }
 
     return new TY::TyList( ty->ActualTy(), make_formal_tylist( tenv, params->tail ) );
@@ -193,27 +195,20 @@ static TY::FieldList* make_fieldlist( TEnvType tenv, A::FieldList* fields ) {
     TY::Ty* ty = tenv->Look( fields->head->typ );
     std::cout << " make_fieldlist kind called. " << fields->head->name->Name() << std::endl;
     if ( !ty ) {
-        std::cout << "undefined type " << fields->head->typ->Name() << std::endl;
+        std::cout << " undefined type " + fields->head->typ->Name() << std::endl;
     }
     return new TY::FieldList( new TY::Field( fields->head->name, ty ), make_fieldlist( tenv, fields->tail ) );
 }
 
-static TY::FieldList* make_fieldlist_from_e( TEnvType tenv, A::EFieldList* fields ) {
+static TY::FieldList* make_fieldlist_from_e( TEnvType tenv, A::EFieldList* fields, TR::Level* level, TEMP::Label* label ) {
     if ( fields == nullptr ) {
         return nullptr;
     }
 
-    TY::Ty* ty = fields->head->exp->SemAnalyze( nullptr, tenv, 0 );
+    TY::Ty* ty = fields->head->exp->Translate( nullptr, tenv, level, label ).ty;
     std::cout << " make_fieldlist kind called. " << fields->head->name->Name() << std::endl;
 
-    return new TY::FieldList( new TY::Field( fields->head->name, ty ), make_fieldlist_from_e( tenv, fields->tail ) );
-}
-
-static TR::ExExp find_static_link( TR::Level* curlevel, TR::Level* declevel ) {
-    while ( curlevel != declevel ) {
-        curlevel        = curlevel->parent;
-        TR::Access* acl = curlevel->Formals( curlevel )->head;
-    }
+    return new TY::FieldList( new TY::Field( fields->head->name, ty ), make_fieldlist_from_e( tenv, fields->tail, level, label ) );
 }
 
 }  // namespace TR
@@ -224,7 +219,7 @@ TR::ExpAndTy SimpleVar::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::
     std::cout << "Entered SimpleVar::Translate." << std::endl;
     E::EnvEntry* var = venv->Look( this->sym );
     if ( var && var->kind == E::EnvEntry::Kind::VAR ) {
-        return TR::ExpAndTy( new TR::ExExp( new T::NameExp( label ) ), ( ( E::VarEntry* )var )->ty );
+        return TR::ExpAndTy( nullptr, ( ( E::VarEntry* )var )->ty );
     }
 
     std::cout << "undefined variable " + this->sym->Name() << std::endl;
@@ -235,12 +230,48 @@ TR::ExpAndTy SimpleVar::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::
 
 TR::ExpAndTy FieldVar::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered SimpleVar::Translate." << std::endl;
+    auto recT = this->var->Translate( venv, tenv, level, label );
+    // if ( recT->kind == E ) {
+    auto recEnt = ( E::VarEntry* )recT.ty;
+
+    if ( recT.ty->kind != TY::Ty::Kind::RECORD ) {
+        std::cout << "not a record type" << std::endl;
+    }
+
+    auto fields = ( ( TY::RecordTy* )( ( A::SimpleVar* )this->var )->Translate( venv, tenv, level, label ).ty )->fields;
+    while ( fields ) {
+        std::cout << "checking field " << fields->head->name->Name() << " and " << this->sym->Name() << std::endl;
+        if ( fields->head->name->Name() == this->sym->Name() ) {
+            return TR::ExpAndTy( nullptr, fields->head->ty );
+        }
+        fields = fields->tail;
+    }
+
+    std::cout << "field " + this->sym->Name() + " doesn't exist" << std::endl;
     return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
 }
 
 TR::ExpAndTy SubscriptVar::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered SubscriptVar::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+
+    auto recT = this->var->Translate( venv, tenv, level, label );
+    // if ( recT->kind == E ) {
+    auto recEnt = recT.ty;
+
+    std::string type_names[] = { "SIMPLE", "FIELD", "SUBSCRIPT" };
+
+    if ( this->var->kind != A::Var::Kind::SUBSCRIPT ) {
+        std::cout << "this->var->kind = " << type_names[ this->var->kind ] << std::endl;
+        std::cout << "array type required" << std::endl;
+    }
+
+    std::string ty_names[] = { "RECORD", "NIL", "INT", "STRING", "ARRAY", "NAME", "VOID" };
+    std::cout << "before we call recEnt->ty->kind" << std::endl;
+    std::cout << recEnt->kind << std::endl;
+    std::cout << "recEnt->ty->kind = " << ty_names[ recEnt->kind ] << std::endl;
+    if ( recEnt->kind == TY::Ty::Kind::ARRAY ) {
+        return TR::ExpAndTy( nullptr, ( ( TY::ArrayTy* )( recEnt ) )->ty );
+    }
 }
 
 TR::ExpAndTy VarExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
@@ -266,26 +297,59 @@ TR::ExpAndTy VarExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty 
 
 TR::ExpAndTy NilExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered NilExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+    return TR::ExpAndTy( nullptr, TY::NilTy::Instance() );
 }
 
 TR::ExpAndTy IntExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered IntExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+    return TR::ExpAndTy( nullptr, TY::IntTy::Instance() );
 }
 
 TR::ExpAndTy StringExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered StringExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+    return TR::ExpAndTy( nullptr, TY::StringTy::Instance() );
 }
 
 TR::ExpAndTy CallExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered CallExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+
+    auto fun = venv->Look( this->func );
+    if ( !fun || fun->kind != E::EnvEntry::Kind::FUN ) {
+        std::cout << "undefined function " + this->func->Name() << std::endl;
+        return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+    }
+    auto        formals = ( ( E::FunEntry* )fun )->formals;
+    A::ExpList* arg     = this->args;
+    TY::Ty*     retType;
+    while ( arg || formals ) {
+        if ( arg && formals ) {
+            std::cout << "comparing arg->head and formals->head" << std::endl;
+            if ( !arg->head->Translate( venv, tenv, level, label ).ty->IsSameType( formals->head ) ) {
+                std::cout << "para type mismatch" << std::endl;
+                break;
+            }
+            arg     = arg->tail;
+            formals = formals->tail;
+        }
+        else {
+            if ( arg ) {
+                std::cout << "too many params in function " + this->func->Name() << std::endl;
+            }
+            else {
+                std::cout << "too less params in function " + this->func->Name() << std::endl;
+            }
+            break;
+        }
+    }
+
+    auto result = ( ( E::FunEntry* )fun )->result;
+    std::cout << "Call operation returns an " << result->kind << std::endl;
+    return TR::ExpAndTy( nullptr, result );
 }
 
 TR::ExpAndTy OpExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
-    std::cout << "Entered OpExp::Translate." << std::endl;
+    std::string op_names[] = { "PLUS_OP", "MINUS_OP", "TIMES_OP", "DIVIDE_OP", "EQ_OP", "NEQ_OP", "LT_OP", "LE_OP", "GT_OP", "GE_OP" };
+    std::cout << "Entered OpExp::Translate. OpCode: " << op_names[ this->oper ] << std::endl;
 
     auto leftT  = left->Translate( venv, tenv, level, label );
     auto rightT = right->Translate( venv, tenv, level, label );
@@ -323,7 +387,11 @@ TR::ExpAndTy OpExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >
 
 TR::ExpAndTy RecordExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered RecordExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+    auto recT = tenv->Look( this->typ );
+    if ( !recT ) {
+        std::cout << "undefined type rectype" << std::endl;
+    }
+    return TR::ExpAndTy( nullptr, new TY::RecordTy( make_fieldlist_from_e( tenv, this->fields, level, label ) ) );
 }
 
 TR::ExpAndTy SeqExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
@@ -360,7 +428,7 @@ TR::ExpAndTy AssignExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::
 }
 
 TR::ExpAndTy IfExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
-    std::cout << "Entered IfExp::Translate." << std::endl;
+    std::cout << "Entered IfExp::Translate. sign: " << std::endl;
     if ( this->test->Translate( venv, tenv, level, label ).ty->kind != TY::Ty::Kind::INT ) {
         return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
     }
@@ -388,7 +456,7 @@ TR::ExpAndTy WhileExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::T
     if ( expAndTy.ty->kind != TY::Ty::Kind::VOID ) {
         std::cout << "while body must produce no value" << std::endl;
     }
-    return expAndTy;
+    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
 }
 
 TR::ExpAndTy ForExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
@@ -414,18 +482,22 @@ TR::ExpAndTy LetExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty 
     A::DecList* node = this->decs;
     while ( node ) {
         // std::cout << "while!" << std::endl;
-        node->head->Translate( venv, tenv, TR::Level::NewLevel( level, label, nullptr ), TEMP::NewLabel() );
+        node->head->Translate( venv, tenv, level, label );
         node = node->tail;
     }
 
-    return this->body->Translate( venv, tenv, TR::Level::NewLevel( level, label, nullptr ), TEMP::NewLabel() );
+    return this->body->Translate( venv, tenv, level, label );
 
     // return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
 }
 
 TR::ExpAndTy ArrayExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered ArrayExp::Translate." << std::endl;
-    return TR::ExpAndTy( nullptr, TY::VoidTy::Instance() );
+
+    if ( !tenv->Look( this->typ )->IsSameType( this->init->Translate( venv, tenv, level, label ).ty ) ) {
+        std::cout << "type mismatch" << std::endl;
+    }
+    return TR::ExpAndTy( nullptr, new TY::ArrayTy( tenv->Look( this->typ ) ) );
 }
 
 TR::ExpAndTy VoidExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
@@ -434,7 +506,49 @@ TR::ExpAndTy VoidExp::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty
 }
 
 TR::Exp* FunctionDec::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
-    std::cout << "Entered FunctionExp::Translate." << std::endl;
+    std::cout << "Entered FunctionDec::Translate." << std::endl;
+
+    FunDecList* func = this->functions;
+    while ( func ) {
+        auto head = func->head;
+        if ( venv->Look( head->name ) ) {
+            std::cout << "two functions have the same name" << std::endl;
+        }
+        if ( head->result ) {
+            venv->Enter( head->name, new E::FunEntry( TR::make_formal_tylist( tenv, head->params ), tenv->Look( head->result ) ) );
+        }
+        else {
+            venv->Enter( head->name, new E::FunEntry( TR::make_formal_tylist( tenv, head->params ), TY::VoidTy::Instance() ) );
+        }
+        if ( func->tail ) {
+            venv->Enter( func->tail->head->name, new E::FunEntry( nullptr, TY::VoidTy::Instance() ) );
+        }
+        auto   field           = head->params;
+        size_t inputParamCount = 0;
+        while ( field ) {
+            auto fh = field->head;
+            venv->Enter( fh->name, new E::VarEntry( tenv->Look( fh->typ ) ) );
+            std::cout << "constructing formal: " << fh->name->Name() << ", type: " << fh->typ->Name() << std::endl;
+            field = field->tail;
+            ++inputParamCount;
+        }
+        auto retType = head->body->Translate( venv, tenv, level, label ).ty;
+
+        field = head->params;
+        while ( inputParamCount > 0 ) {
+            std::cout << "try pop one out" << std::endl;
+            venv->Pop();
+
+            --inputParamCount;
+        }
+        std::cout << "function declared result: " << ( head->result ? head->result->Name() : "NULL" ) << std::endl;
+        if ( ( ( E::FunEntry* )( venv->Look( head->name ) ) )->result->kind == TY::Ty::Kind::VOID && retType->kind != TY::Ty::Kind::VOID ) {
+            std::cout << "procedure returns value" << std::endl;
+        }
+        std::cout << "head->result: " << head->result << ", retType->kind: " << retType->kind << std::endl;
+
+        func = func->tail;
+    }
     return nullptr;
 }
 
@@ -463,26 +577,96 @@ TR::Exp* VarDec::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* t
         }
         venv->Enter( this->var, new E::VarEntry( initT.ty ) );
     }
+    return nullptr;
 }
 
 TR::Exp* TypeDec::Translate( S::Table< E::EnvEntry >* venv, S::Table< TY::Ty >* tenv, TR::Level* level, TEMP::Label* label ) const {
     std::cout << "Entered TypeDec::Translate." << std::endl;
+    // std::cout << "Entered TypeDec::SemAnalyse; labelcount: " << labelcount << std::endl;
+    // NameAndTyList*           natl = this->types;
+    // TY::Ty*                  last = nullptr;
+    // std::vector< NameAndTy > nstack;
+    // while ( natl ) {
+    //     nstack.push_back( natl->head );
+    //     natl = natl->tail;
+    // }
+    // size_t lastEnds = nstack.size
+    // while ( natl != nullptr ) {
+    // std::cout << "gonna declare type " << natl->head->name->Name() << std::endl;
+    // if ( natl->head->ty->SemAnalyze( tenv ) ) {
+    //     tenv->Enter( natl->head->name, natl->head->ty->SemAnalyze( tenv ) );
+    //     }
+    //     natl = natl->tail;
+    // }
+
+    NameAndTyList*           natl = this->types;
+    std::deque< NameAndTy* > queue;
+    while ( natl ) {
+        queue.push_back( natl->head );
+        natl = natl->tail;
+    }
+    if ( queue.size() > 1 ) {
+        std::cout << "queue.size() > 1" << std::endl;
+        size_t unmovedTimes = 0;
+        while ( unmovedTimes < queue.size() && queue.size() ) {
+            auto nt = queue[ 0 ];
+            queue.pop_front();
+            // tenv->Enter( nt->name, TY::NilTy::Instance() );
+            auto ans = nt->ty->Translate( tenv );
+            if ( ans ) {
+                unmovedTimes = 0;
+                if ( tenv->Look( nt->name ) ) {
+                    std::cout << "two types have the same name" << std::endl;
+                }
+                tenv->Enter( nt->name, ans );
+            }
+            else {
+                queue.push_back( nt );
+                unmovedTimes += 1;
+            }
+            std::cout << "declared type " << nt->name->Name() << std::endl;
+        }
+        if ( queue.size() ) {
+            std::cout << "illegal type cycle" << std::endl;
+        }
+    }
+    else {
+        auto nt = queue[ 0 ];
+        tenv->Enter( nt->name, TY::NilTy::Instance() );
+        std::cout << "queue.size() <= 1" << std::endl;
+
+        auto ans = nt->ty->Translate( tenv );
+        if ( ans ) {
+            tenv->Set( nt->name, ans );
+        }
+        else {
+            std::cout << " undefined type " << std::endl;
+        }
+        std::cout << "declared type " << nt->name->Name() << std::endl;
+    }
+
     return nullptr;
 }
 
 TY::Ty* NameTy::Translate( S::Table< TY::Ty >* tenv ) const {
     std::cout << "Entered NameTy::Translate." << std::endl;
-    return TY::VoidTy::Instance();
+    auto ty = tenv->Look( this->name );
+    if ( ty ) {
+        return ty;
+    }
+    else {
+        std::cout << " undefined type " + this->name->Name() << std::endl;
+    }
 }
 
 TY::Ty* RecordTy::Translate( S::Table< TY::Ty >* tenv ) const {
     std::cout << "Entered RecordTy::Translate." << std::endl;
-    return TY::VoidTy::Instance();
+    return new TY::RecordTy( TR::make_fieldlist( tenv, this->record ) );
 }
 
 TY::Ty* ArrayTy::Translate( S::Table< TY::Ty >* tenv ) const {
     std::cout << "Entered ArrayTy::Translate." << std::endl;
-    return TY::VoidTy::Instance();
+    return tenv->Look( this->array );
 }
 
 }  // namespace A
