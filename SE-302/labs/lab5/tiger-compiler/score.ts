f5825d826623f5bf40820a250dfcc573ea1d42bb
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-15 14:41:54 +0800

    gotta 10/100

---
+++ SE-302/labs/lab5/tiger-compiler/score.ts
@@ -0,0 +1,18 @@
+[1;34m[*_*]: Output mismatches. [bsearch.tig][0m
+[1;34m[*_*]: Output mismatches. [dec2bin.tig][0m
+[1;34m[*_*]: Output mismatches. [merge.tig][0m
+[1;34m[*_*]: Output mismatches. [merge.tig][0m
+[1;34m[*_*]: Output mismatches. [merge.tig][0m
+[1;34m[*_*]: Output mismatches. [merge.tig][0m
+[1;34m[*_*]: Output mismatches. [prime.tig][0m
+[1;34m[*_*]: Output mismatches. [qsort.tig][0m
+[1;34m[*_*]: Output mismatches. [queens.tig][0m
+[1;34m[*_*]: Output mismatches. [tbi.tig][0m
+[1;34m[*_*]: Output mismatches. [tfact.tig][0m
+pass tfo.tig
+[1;34m[*_*]: Output mismatches. [tif.tig][0m
+[1;34m[*_*]: Output mismatches. [tifn.tig][0m
+[1;34m[*_*]: Output mismatches. [tlink.tig][0m
+[1;34m[*_*]: Output mismatches. [trec.tig][0m
+pass twhi.tig
+[1;32mYour score: 10[0m
