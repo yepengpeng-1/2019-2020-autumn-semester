085b455fb060f43c1306239825fea91b54745530
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-15 18:45:41 +0800

    got merely 15. should throw myself into gutter and feed on trash

--- SE-302/labs/lab5/tiger-compiler/score.ts
+++ SE-302/labs/lab5/tiger-compiler/score.ts
@@ -13,6 +13,6 @@ pass tfo.tig
 [1;34m[*_*]: Output mismatches. [tif.tig][0m
 [1;34m[*_*]: Output mismatches. [tifn.tig][0m
 [1;34m[*_*]: Output mismatches. [tlink.tig][0m
-[1;34m[*_*]: Output mismatches. [trec.tig][0m
+pass trec.tig
 pass twhi.tig
-[1;32mYour score: 10[0m
+[1;32mYour score: 15[0m
