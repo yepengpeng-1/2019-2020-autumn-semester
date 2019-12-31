197e255188611eac36464cc97066a1efe758c207
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-31 19:23:07 +0800

    [add] Catalina docker running scripts

---
+++ SE-302/labs/run_docker_lab5_catalina.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash 
+
+docker run -it --privileged -v "/Volumes/Macintosh HD (Sierra)/Users/yue/Documents/GitHub/2019-2020-autumn-semester/SE-302/labs/lab5/tiger-compiler":/home/stu/tiger-compiler-2019-fall se302/tigerlabs_env:latest /bin/bash
