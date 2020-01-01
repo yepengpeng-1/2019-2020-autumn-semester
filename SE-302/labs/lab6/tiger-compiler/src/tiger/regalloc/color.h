9bfd7fcf033a213e2073eef54bad3d7d890d419e
yuetsin <akaza_akari@sjtu.edu.cn>
2020-01-01 10:06:15 +0800

    [add] data structures

--- SE-302/labs/lab6/tiger-compiler/src/tiger/regalloc/color.h
+++ SE-302/labs/lab6/tiger-compiler/src/tiger/regalloc/color.h
@@ -5,6 +5,8 @@
 #include "tiger/liveness/liveness.h"
 #include "tiger/util/graph.h"
 
+#include <vector>
+
 namespace COL {
 
 class Result {
@@ -13,8 +15,32 @@ public:
     TEMP::TempList* spills;
 };
 
-Result Color( G::Graph< TEMP::Temp >* ig, TEMP::Map* initial, TEMP::TempList* regs, LIVE::MoveList* moves );
+typedef G::Node< TEMP::Temp >             tempNode;
+typedef std::pair< tempNode*, tempNode* > Move;
+typedef Move                              Edge;
+Result                                    Color( G::Graph< TEMP::Temp >* ig, TEMP::Map* initial, TEMP::TempList* regs, LIVE::MoveList* moves );
 
+static std::set< tempNode > precolored;
+static std::set< tempNode > initial;
+static std::set< tempNode > simplifyWorklist;
+static std::set< tempNode > freezeWorkList;
+static std::set< tempNode > spillWorkList;
+static std::set< tempNode > spilledNodes;
+static std::set< tempNode > coalescedNodes;
+static std::set< tempNode > selectedStack;
+
+static std::set< Move > coalescedMoves;
+static std::set< Move > constrainedMoves;
+static std::set< Move > frozenMoves;
+static std::set< Move > worklistMoves;
+static std::set< Move > activeMoves;
+
+static std::set< Edge >                           adjSet;
+static std::map< tempNode, std::set< tempNode > > adjList;
+static std::map< tempNode, size_t >               degree;
+static std::map< tempNode, std::set< Move > >     moveList;
+static std::map< tempNode, tempNode >             alias;
+static std::map< tempNode, std::string* >         color;
 }  // namespace COL
 
 #endif
